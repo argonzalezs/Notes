02965be97e69dcc78d2d7d2ab41ab8a2b7ebc0d0
Adolfo <argonzalezs@gmail.com>
2023-03-07 15:09:47 -0400

    Add project files.

---
+++ Notes/Models/Note.cs
@@ -0,0 +1,10 @@
+﻿namespace Notes.Models
+{
+    internal class Note
+    {
+        public string Filename { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+    }
+
+}
